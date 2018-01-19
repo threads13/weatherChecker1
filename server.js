c6f1f6ee39b442a9233d8665e3cf5b8cafed1cbb
Jacob Hoke <jacob@threads13.com>
2018-01-19 01:57:12 -0500

    config for heroku push

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require("express");
-
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+	if(){
+		if(req.headers['x-forwarded-proto"] === "http"){
+			next();
+		} else {
+		 res.redirect("http://" + req.hostname + req.url);
+		}
+});
 
 app.use(express.static("public"));
 
-app.listen(3000, function(){
-	console.log("server is running");
-});
+app.listen(port, function(){
+	console.log("server is running on port" + PORT);
+}); 
