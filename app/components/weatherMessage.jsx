f8b031dd485b39740b97466de73afb328fb98b04
Jacob Hoke <jacob@threads13.com>
2018-01-18 21:08:26 -0500

    initial commit

---
+++ app/components/weatherMessage.jsx
@@ -0,0 +1,16 @@
+var React = require("react");
+
+var WeatherMessage = React.createClass({
+		render: function(){
+			var {temp, location} = this.props; 
+			
+			return(
+				<div>
+					<h3>It is {temp} in {location}</h3>
+
+				</div>
+		);
+	}
+});
+
+module.exports = WeatherMessage;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+		root: __dirname,
+		alias: {
+			Main: "app/components/main.jsx",
+			Nav: "app/components/nav.jsx",
+			Weather: "app/components/weather.jsx",
+			About: "app/components/about.jsx",
+			Examples: "app/components/examples.jsx",
+			WeatherForm: "app/components/weatherForm.jsx",
+			WeatherMessage: "app/components/weatherMessage.jsx",
+			openWeatherMap: "app/api/openWeatherMap.jsx"
+		},
+    extensions: ['', '.js', '.jsx']
+  },
+    module: { 
+        loaders: [
+            {
+                loader: "babel-loader",
+                query: {
+                presets: ["react", "es2015"]
+                },
+								test: /\.jsx?$/,
+								exclude: /(node_modules|bower_components)/
+            }      
+        ]
+    }
+};
